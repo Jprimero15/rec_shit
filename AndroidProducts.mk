c6cb2d6ee45f77b228a5fbbf6de38c9504b9accd
Jprimero15 <jprimero155@gmail.com>
2022-04-11 08:52:01 +0000

    init dt
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_olive.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_olive-user \
+    omni_olive-userdebug \
+    omni_olive-eng
